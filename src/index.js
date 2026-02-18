f39362dc358e4ca01b01bbfabf0f20f21b5f9f5b
PJYUCATAN\alejandro.gongora <alejandro.gongora@pjyucatan.gob.mx>
2026-02-18 13:30:09 -0600

    Modificación de mensaje

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const app = express();
 app.get('/',(request, response)=>{
-    response.send('Hola mundo');
+    response.send('Hola mundo, mensaje de inicio');
 });
 app.listen(3000,()=>{
     console.log('App listening on port 3000');
